900cafaee91e4a524d4aac01de08f89749cc8ee5
Nero Lu <cbhb4509@gmail.com>
2021-07-13 15:05:12 +0800

    Try

--- TryGithubForm/TryGithubForm/Program.cs
+++ TryGithubForm/TryGithubForm/Program.cs
@@ -17,6 +17,7 @@ namespace TryGithubForm
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //QQWSDSFAFSFCXCXC
         }
     }
 }
